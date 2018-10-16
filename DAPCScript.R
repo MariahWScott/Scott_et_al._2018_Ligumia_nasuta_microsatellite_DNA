9ab3e73a6d28aa41d220e70f2e044235f0c8ef40
MariahWScott <33735003+MariahWScott@users.noreply.github.com>
2018-10-15 21:15:47 -0500

    Update DAPCScript.R

--- DAPCScript.R
+++ DAPCScript.R
@@ -16,14 +16,18 @@ define_and_plot_cluster <- function(file_name, n_max_clusters){
   group <- find.clusters(obj1, max.n.clust = n_max_clusters)
   #Retain 200 PCs (or over all) and choose the number of clusters on the elbow of the BIC graph
   dapc1 <- dapc(obj1, group$grp)
-  #Retained informative PCs, all eigenvalues retained (so 15 to be safe)
+  #Retained informative PCs (we used 100), all eigenvalues retained (so 8 to be safe)
   compoplot(dapc1,
             txt.leg=paste("Cluster", 1:9), lab="",
             ncol=1, xlab="individuals", col=rainbow(9))
+  scatter(dapc1, posi.da="bottomright", bg="white", pch=20, cstar=0, 
+          col=rainbow(9), scree.da=TRUE, posi.pca="bottomleft", cell=0, 
+          cex=1.5, clab=0, txt.leg=paste("Cluster",1:9), scree.pca=TRUE)
+  #Need to make number of colors in scatter plot match the number of PC clusters used
   return(print("Done!"))
 }
 
 #Run the above function for each file desired, with the number of maximum clusters defined
 #Typically you will want the number of maximum clusters the number of sites sampled + 1
 #Look at the comments in the above function for suggestions on how to respond to the prompts in the console
-define_and_plot_cluster(file_name = "Example_file.gen", n_max_clusters = 41)
+define_and_plot_cluster(file_name = "GenePop_April_9_K_is_57.gen", n_max_clusters = 41)
