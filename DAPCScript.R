bb9cd6a69462cae9b3d9e5637e0e0fe636716ad7
MariahWScott <33735003+MariahWScott@users.noreply.github.com>
2018-10-26 15:13:08 -0500

    Update DAPCScript.R

--- DAPCScript.R
+++ DAPCScript.R
@@ -20,9 +20,14 @@ define_and_plot_cluster <- function(file_name, n_max_clusters){
   compoplot(dapc1,
             txt.leg=paste("Cluster", 1:9), lab="",
             ncol=1, xlab="individuals", col=rainbow(9))
+  #Multiple Scatter Plots of different formats
   scatter(dapc1, posi.da="bottomright", bg="white", pch=20, cstar=0, 
           col=rainbow(9), scree.da=TRUE, posi.pca="bottomleft", cell=0, 
           cex=1.5, clab=0, txt.leg=paste("Cluster",1:9), scree.pca=TRUE)
+  scatter(dapc1, posi.da="bottomright", bg="white",
+          pch=20, cstar=0, col=rainbow(9), scree.pca=TRUE,
+          posi.pca="bottomleft", clab=0.50)
+  scatter(dapc1, pch=20, cstar=0, col=rainbow(9), clab=0.5)
   #Need to make number of colors in scatter plot match the number of PC clusters used
   return(print("Done!"))
 }
